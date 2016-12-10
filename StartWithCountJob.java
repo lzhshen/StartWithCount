0719fd0b80ce482f06a6544595b8d12d646daff3
lzhshen <lzhshen@hotmail.com>
2016-12-10 11:58:22 +0800

    Add files via upload

---
+++ StartWithCountJob.java
@@ -0,0 +1,35 @@
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.util.Tool;
+
+public class StartWithCountJob extends Configured implements Tool {
+	
+  @Override
+  public int run(String[] args) throws Exception {
+    Job job = Job.getInstance(getConf(), "StartsWithCount");
+	job.setJarByClass(getClass());
+	// configure output and input source
+	TextInputFormat.addInputPath(job, new Path(args[0]));
+	job.setInputFormatClass(TextInputFormat.class);
+	// configure mapper and reducer
+	job.setMapperClass(StartWithCountMapper.class);
+	job.setCombinerClass(StartWithCountReducer.class);
+	job.setReducerClass(StartWithCountReducer.class);
+	// configure output
+	TextOutputFormat.setOutputPath(job, new Path(args[1]));
+	job.setOutputFormatClass(TextOutputFormat.class);
+	job.setOutputKeyClass(Text.class);
+	job.setOutputValueClass(IntWritable.class);
+	return job.waitForCompletion(true) ? 0 : 1;
+  }
+	
+  public static void main(String[] args) throws Exception {
+    int exitCode = ToolRunner.run(new StartWithCountJob(), args);
+    System.exit(exitCode);
+  }
+}
